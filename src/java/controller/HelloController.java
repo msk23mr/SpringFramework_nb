006f43dd7edc183a7593750f85447ca1d02dc13f
masaki <marlcola2@gmail.com>
2015-03-27 18:13:01 +0900

    [modify] onSubmitメソッドを定義

--- src/java/controller/HelloController.java
+++ src/java/controller/HelloController.java
@@ -5,13 +5,24 @@
  */
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
+import service.HelloService;
 
 /**
  *
  * @author mori
  */
 public class HelloController extends SimpleFormController {
+    
+    private HelloService helloService;
+    
+    public void setHelloService (HelloService helloService) {
+        this.helloService = helloService;
+    }
     
     public HelloController() {
         //Initialize controller properties here or 
@@ -22,24 +33,18 @@ public class HelloController extends SimpleFormController {
         setSuccessView("helloView");
         setFormView("nameView");
     }
-    
+
     @Override
-    protected void doSubmitAction(Object command) throws Exception {
-        throw new UnsupportedOperationException("Not yet implemented");
+    protected ModelAndView onSubmit(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object command,
+            BindException errors) throws Exception {
+        
+        Name name = (Name) command;
+        ModelAndView mv = new ModelAndView(getSuccessView());
+        mv.addObject("helloMessage", helloService.sayHello(name.getValue()));
+        return mv;
     }
 
-    //Use onSubmit instead of doSubmitAction 
-    //when you need access to the Request, Response, or BindException objects
-    /*
-     @Override
-     protected ModelAndView onSubmit(
-     HttpServletRequest request, 
-     HttpServletResponse response, 
-     Object command, 
-     BindException errors) throws Exception {
-     ModelAndView mv = new ModelAndView(getSuccessView());
-     //Do something...
-     return mv;
-     }
-     */
 }
